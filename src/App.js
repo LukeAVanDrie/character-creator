49b332d194feb2e06fc7a97791dd8cba31dc88bf
Van Drie <lvandrie@DMSis-MacBook-Pro.local>
2020-05-18 15:40:04 -0500

    converted app to functional component

--- src/App.js
+++ src/App.js
@@ -8,18 +8,16 @@ import {
 import CharacterForm from "./components/characterForm";
 import CharacterSheet from "./components/characterSheet";
 
-export default class App extends React.Component {  
-  render() {
-    return (
-      <Router>
-        <Redirect from="/" exact to="/character_sheet"/>
-        <div>
-          <Switch>
-            <Route exact path="/character_form" component={ CharacterForm }/>
-            <Route exact path="/character_sheet" component={ CharacterSheet }/>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+export default function App() {  
+  return (
+    <Router>
+      <Redirect from="/" exact to="/character_sheet"/>
+      <div>
+        <Switch>
+          <Route exact path="/character_form" component={ CharacterForm }/>
+          <Route exact path="/character_sheet" component={ CharacterSheet }/>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
